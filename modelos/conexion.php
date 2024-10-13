d3e6836f0da3924623db64abde2afa33755c2bca
Ivan Valera <ivanvaleraa@gmail.com>
2024-10-13 19:49:24 -0400

    adding connection again

---
+++ modelos/conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+class Conexion{
+    //actualizacion para poder conseguir el lastId. El codigo viejo no lo conseguia porque cada vez
+    //que se llamaba al metodo static se creaba una nueva llamada y por lo tanto una nueva sesion,
+    //impidiendo recuperar el lastId de la sesion que ya estaba abierta, por lo tanto retornaba 0.
+    public static $link;
+    public static function conectar(){
+        if(!self::$link){
+            self::$link = new PDO( 'mysql:host=127.0.0.1:3306;dbname=adxhbkeexw',
+                'adxhbkeexw',
+                '3ZkFSqtv27');
+        }
+		return self::$link;
+	}
+
+}
